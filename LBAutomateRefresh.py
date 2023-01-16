36088692bb47b556b51198040a2c55e0bedc7332
app1778 <app1778@yahoo.com>
2023-01-15 16:15:24 -0800

    Added vintage info parsing. Randomized timing interval for site pings. Cleaned up code.

--- LBAutomateRefresh.py
+++ LBAutomateRefresh.py
@@ -1,13 +1,25 @@
 from LBTracker import LBTracker
+from random import randrange
 
 import time
 
-refresh_rate = input("Enter a refresh frequency (seconds): ")
-refresh_rate = int(refresh_rate)
+# Randomize the interval in which to check site
+max_int = input("Enter a maximum time to check (seconds): ")
+max_int = int(max_int)
+
+# Hold current wine to compare if a new one is posted
+current_wine = ''
 
 while True:
-	time.sleep(refresh_rate)
+	time.sleep(randrange(max_int))
+	
 	tracker = LBTracker()
 	wine = tracker.get_wine()
 	price = tracker.get_price()
-	print(wine + " - $" + price)
+	vintage = tracker.get_vintage()
+
+	if wine != current_wine:
+		current_wine = wine
+		print(wine)
+		print("\tPrice = $" + price)
+		print("\tVintage = " + vintage)
--- LBTracker.py
+++ LBTracker.py
@@ -1,13 +1,19 @@
 from bs4 import BeautifulSoup
 
+import re
 import requests
+
+LB_URL = 'https://www.lastbottlewines.com'
+LB_OFFER_NAME_TAG = 'offer-name'
+LB_PRICE_TAG = 'amount lb'
 
 class LBTracker():
 
 	def __init__(self):
-		self.url = 'https://www.lastbottlewines.com'
+		self.url = LB_URL
 		self.wine = ''
 		self.price = ''
+		self.year = ''
 
 	def __str__(self):
 		return "LastBottleWines.com tracker."
@@ -22,10 +28,29 @@ class LBTracker():
 
 	def get_soup(self):
 		source = requests.get(self.url).text
-		soup = BeautifulSoup(source, 'lxml')
+		soup = BeautifulSoup(source, features="html.parser")
 		return soup
 
 	def parse_soup(self):
 		soup = self.get_soup()
-		self.wine = soup.find('h1', {'class': 'offer-name'}).text.strip()
-		self.price = soup.find('span', {'class': 'amount lb'}).text.strip()
+		self.wine = soup.find('h1', {'class': LB_OFFER_NAME_TAG}).text.strip()
+		self.price = soup.find('span', {'class': LB_PRICE_TAG}).text.strip()
+
+	# parse vintage year from wine name
+	def get_vintage(self):
+
+		# find "20" or "19" as start of vintage years
+		match = re.search("20|19", self.wine)
+
+		if match is not None:
+			start = match.span()[0]
+
+			# assume full year is listed
+			end = start + 4
+
+			print("index of start = " + str(start))
+			print("index of end = " + str(end))
+			self.year = self.wine[start:end]
+
+		return self.year
+
